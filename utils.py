3b2446e68c12ccec9ae575f84de3ec5b3e3d0429
dmisino <dmisino@gmail.com>
2023-05-11 03:30:08 -0700

    Initial commit, fairly complete in one session

---
+++ utils.py
@@ -0,0 +1,27 @@
+import os
+
+def get_openai_gpt35_cost(total_tokens):
+  total_cost = total_tokens/1000 * 0.002
+  return total_cost
+
+def print_without_cutting_words(text):
+  cols, rows = os.get_terminal_size()
+  text = text.rstrip('\n')  # Remove any trailing newline character
+  lines = text.split('\n')
+  new_lines = []
+  for line in lines:
+    line_length = 0
+    new_line = []
+    for word in line.split():
+      if not word:
+        continue
+      word_length = len(word)
+      if line_length + word_length + 1 <= cols:
+        new_line.append(word)
+        line_length += word_length + 1
+      else:
+        new_lines.append(' '.join(new_line))
+        new_line = [word]
+        line_length = word_length
+    new_lines.append(' '.join(new_line))
+  return '\n'.join(new_lines)
